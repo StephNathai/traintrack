e092b7f03de72d3d063aa35cb56c2fd3b28f484a
Nathai <spontandom@gmail.com>
2015-10-31 22:31:26 -0400

    moved morgan to dependencies bc heroku logs said cant find it and installed again

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var stopsShuttle = require('./public/stopsShuttle.js')
 var server = app.listen(3000 || process.env.PORT)
 var io     = require('socket.io')(server);
 
-///app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use( express.static( 'public') );
 
 io.on('connection', function(socket) {
